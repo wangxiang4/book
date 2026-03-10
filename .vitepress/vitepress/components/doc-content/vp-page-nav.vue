00039036ea0dc9a83503aa1ff6d0c67d8909eaf7
wangxiang4 <github.wangxiang4@gmail.com>
2026-03-11 04:00:51 +0800

    fix: pagination space between

--- .vitepress/vitepress/components/doc-content/vp-page-nav.vue
+++ .vitepress/vitepress/components/doc-content/vp-page-nav.vue
@@ -8,87 +8,82 @@
 </script>
 
 <template>
-  <div v-if="hasLinks" class="next-and-prev-link">
-    <div class="container">
-      <div class="prev">
-        <a v-if="prev" class="link" :href="withBase(prev.link)">
-          <i-msl-arrow-back-2-outline-rounded class="nav-icon" />
-          <ClientOnly>
-            <ElTooltip v-if="isDesktop"
-                       :content="prev.text"
-                       placement="top"
-                       effect="light"
-                       :show-after="600">
-              <span class="text">{{ prev.text }}</span>
-            </ElTooltip>
-            <span v-else class="text">{{ prev.text }}</span>
-          </ClientOnly>
-        </a>
-      </div>
-      <div class="next">
-        <a v-if="next" class="link" :href="withBase(next.link)">
-          <ClientOnly>
-            <ElTooltip v-if="isDesktop"
-                       :content="next.text"
-                       placement="top"
-                       effect="light"
-                       :show-after="600">
-              <span class="text">{{ next.text }}</span>
-            </ElTooltip>
-            <span v-else class="text">{{ next.text }}</span>
-          </ClientOnly>
-          <i-msl-play-arrow-outline-rounded class="nav-icon"/>
-        </a>
-      </div>
+  <div v-if="hasLinks" class="pagination">
+    <div class="btn-prev">
+      <a v-if="prev" class="link" :href="withBase(prev.link)">
+        <i-msl-arrow-back-2-outline-rounded />
+        <ClientOnly>
+          <ElTooltip v-if="isDesktop"
+                     :content="prev.text"
+                     placement="top"
+                     effect="light"
+                     :show-after="600">
+            <span class="text">{{ prev.text }}</span>
+          </ElTooltip>
+          <span v-else class="text">{{ prev.text }}</span>
+        </ClientOnly>
+      </a>
+    </div>
+    <div class="btn-next">
+      <a v-if="next" class="link" :href="withBase(next.link)">
+        <ClientOnly>
+          <ElTooltip v-if="isDesktop"
+                     :content="next.text"
+                     placement="top"
+                     effect="light"
+                     :show-after="600">
+            <span class="text">{{ next.text }}</span>
+          </ElTooltip>
+          <span v-else class="text">{{ next.text }}</span>
+        </ClientOnly>
+        <i-msl-play-arrow-outline-rounded />
+      </a>
     </div>
   </div>
 </template>
 
-<style scoped>
-.next-and-prev-link {
+<style scoped lang="scss">
+.pagination {
   padding: 1rem 0;
-}
-
-.container {
   display: flex;
   justify-content: space-between;
-}
 
-.prev,
-.next {
-  display: flex;
-  flex-shrink: 0;
-  width: 50%;
-}
-
-.prev {
-  justify-content: flex-start;
-}
-
-.next {
-  justify-content: flex-end;
-}
-
-.link {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 100%;
-  height: 24px;
-  font-size: 14px;
-  font-weight: 500;
-
-  .text {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  .btn-prev,
+  .btn-next {
+    display: flex;
+    flex-shrink: 0;
+    width: 50%;
   }
 
-  svg {
-    flex-shrink: 0;
-    font-size: 28px;
-    color: var(--text-color-light);
-    transform: translateY(1px);
+  .btn-prev {
+    justify-content: flex-start;
+  }
+
+  .btn-next {
+    justify-content: flex-end;
+  }
+
+  .link {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    height: 24px;
+    font-size: 14px;
+    font-weight: 500;
+
+    .text {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    svg {
+      flex-shrink: 0;
+      font-size: 28px;
+      color: var(--text-color-light);
+      transform: translateY(1px);
+    }
   }
 }
 </style>
